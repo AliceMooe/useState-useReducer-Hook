11f02f2165c55bc83df29af88c7b85792a7ec868
Hsu Hsu Htet <hsuhsuhtet@Hsus-MacBook-Air.local>
2023-12-21 14:11:42 +0630

    useReducer Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const firstReducer = (state , action) => {
+  switch(action.type){
+    case "minus" : 
+    return {...state,count : state.count - 1 };
+    case "plus" : 
+    return {... state, count : state.count + 1};
+    case "updateKey":
+      return{...state, key : action.payload};
+    default :
+    throw new Error();
+
+}};
+const ACTION = {
+  MINUS: 'minus',
+  PLUS:'plus',
+  UPDATE_KEY:"updateKey",
+}
 
 
 
 function App() {
-  const [key, setKey] = useState("");
-  const [count, setCount] = useState(0);
+  const [state ,dispatch] = useReducer (firstReducer,{key : "" , count : 0});
+  // const [key, setKey] = useState("");
+  // const [count, setCount] = useState(0);
   // useEffect( () => {
   //   console.log("I am use effect hook.");
   // },[]);
@@ -13,17 +32,25 @@ function App() {
     <div className="App">
     
 
-     <input type="text"  onChange={(e) => {
+     {/* <input type="text"  onChange={(e) => {
       setKey(e.target.value)
-     }}/>
-     <h1> Your key is - {key}</h1>
-     <button onClick={() => {
+     }}/> */}
+     {/* <h1> Your key is - {key} </h1> */}
+     {/* <button onClick={() => {
       setCount((prevCount) => prevCount - 1)
-     }}> - </button>
-     <span>{count}</span>
-     <button onClick={() => {
+     }}> - </button> */}
+      {/* <button onClick={() => {
       setCount((prevCount) => prevCount + 1)
-     }}> + </button>
+     }}> + </button> */}
+
+     <input type="text" 
+     onChange={(e) => dispatch({type: ACTION.UPDATE_KEY,payload : e.target.value})} />
+     <h1> Your key is - {state.key} </h1>
+     <button onClick={()=> dispatch({type: ACTION.MINUS})}>-</button>
+     <span>{state.count}</span>
+   
+      <button onClick={()=> dispatch({type: ACTION.PLUS})}>+</button>
+
 
     </div>
   );
